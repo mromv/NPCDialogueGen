42fa200c4e40f92ea8803fd9e9355a567422c60a
mromv <mxromv@gmail.com>
2025-06-25 06:20:32 +0300

    add: model description

--- app/schemas/schema.py
+++ app/schemas/schema.py
@@ -1,16 +1,27 @@
 """
-Базовая модель данных, поддерживающая преобразование в текстовый промпт
+Базовая модель данных, поддерживающая преобразования в текстовый промпт
 """
+from typing import ClassVar
 from typing import List, Dict
 from pydantic import BaseModel
 from pydantic.fields import FieldInfo
 
 
 class AutoPromptModel(BaseModel):
-    _TAB = "  "
-    _BULLET = "- "
+    _TAB: ClassVar[str] = "  "
+    _BULLET: ClassVar[str] = "- "
 
-    def as_prompt(self, *, _lvl: int = 0) -> str:
+    @classmethod
+    def model_description(cls) -> str:
+        """Описание всех полей модели"""
+        lines = []
+        for field_name, field_info in cls.model_fields.items():
+            desc = field_info.description or "Описание отсутствует"
+            lines.append(f"{cls._BULLET}{str(field_name)}: {desc}")
+        return "\n".join(lines)
+
+    def as_prompt(self, exclude_none: bool = True, *, _lvl: int = 0) -> str:
+        """Перевод полей экземпляра класса в текстовое представление для промпта"""
         def pad(bul: str = self._BULLET): return self._TAB * _lvl + bul
             
         parts: List[str] = []
@@ -18,7 +29,7 @@ class AutoPromptModel(BaseModel):
         
         for field_name, field_info in model_fields.items():
             value = getattr(self, field_name)
-            if not value:
+            if exclude_none and not value:
                 continue
 
             description = field_info.description or field_name.replace("_", " ").capitalize()
