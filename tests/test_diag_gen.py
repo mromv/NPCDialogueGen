e0704c2804856616d29c32484b634027d5b98d4e
mromv <mxromv@gmail.com>
2025-06-26 05:22:11 +0300

    upd: validation example

--- tests/test_diag_gen.py
+++ tests/test_diag_gen.py
@@ -1,37 +1,58 @@
 import asyncio
 from app.schemas import (
     Character, Goal, StructureConstraints,
-    TreeGenerationRequest, ContentGenerationRequest,
-    GenerationResponse
+    TreeGenerationRequest, TreeGenerationResponse,
+    ContentGenerationRequest, ContentGenerationResponse,
+    TreeValidationRequest, TreeValidationResponse
 )
-from app.services import TreeGenerator, ContentWriter
+from app.services import (
+    TreeGenerator, ContentWriter, TreeValidator
+)
 
 
 async def test_dialog_generation():
     # generators init
     tree = TreeGenerator()
     content_writer = ContentWriter()
+    tree_validator = TreeValidator()
 
     # input example
     character = Character(**Character.Config.json_schema_extra["example"])
     goal = Goal(**Goal.Config.json_schema_extra["example"])
     tree_constraints = StructureConstraints(max_turns=5)
 
-    tree_request = TreeGenerationRequest(character=character, goal=goal, constraints=tree_constraints)
-    dialog_tree: GenerationResponse = await tree.generate_structure_tree(tree_request)
+    # tree generation
+    tree_request = TreeGenerationRequest(
+        character=character,
+        goal=goal, 
+        onstraints=tree_constraints
+    )
+    dialog_tree: TreeGenerationResponse = await tree.generate_structure_tree(tree_request)
 
+    # nodes content generation
     content_request = ContentGenerationRequest(
         dialog_tree=dialog_tree.dialog_tree,
         character=character,
         goal=goal,
     )
 
-    filled_dialog_tree: GenerationResponse = await content_writer.fill_dialog_tree(
+    filled_dialog_tree: ContentGenerationResponse = await content_writer.fill_dialog_tree(
         request=content_request
     )
 
+    # tree validation
+    tree_validation_request = TreeValidationRequest(
+        character=character,
+        goal=goal,
+        constraints=tree_constraints,
+        dialog_tree=filled_dialog_tree.dialog_tree
+    )
+    tree_validation: TreeValidationResponse = await tree_validator.validate(tree_validation_request)
+
     print(filled_dialog_tree)
+    print(tree_validation)
     return filled_dialog_tree
+
 
 if __name__ == "__main__":
     asyncio.run(test_dialog_generation())
